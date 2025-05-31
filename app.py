4da54df4fc307a9ea8de7c1fad4d5ea32b816d98
sivajanardhan9989@gmail.com <sivajanardhan9989@gmail.com>
2025-05-31 16:35:54 +0530

    docker files for python app using nginx reverse proxy

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('')
-def home()
-    return Hello from Python app behind Nginx reverse proxy!
+@app.route('/')
+def home():
+    return "Hello from Python app behind Nginx reverse proxy!"
 
-if __name__ == __main__
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
